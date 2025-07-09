1e384d618a52bbdc89fa2f6c8b9733e46f184c41
ABHIJIT649 <abhijitmohanty440@gmail.com>
2025-07-09 22:55:23 +0530

    add js

---
+++ script.js
@@ -0,0 +1,6 @@
+const randomNumber = parseInt(Math.random()*100 + 1) // random number generate
+
+const submit = document.querySelector('#subt')
+const userInput = document.querySelector('#guessField')
+const guessSlot = document.querySelector('.Guesses')
+const Reamining = document.querySelector('.lastResult')
